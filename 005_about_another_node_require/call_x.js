a4787e22e5e384f134346e92da449d6360f85c09
Nakada Takumi <usrNeko@kerorin.local>
2017-03-01 20:01:23 +0900

    import

---
+++ 005_about_another_node_require/call_x.js
@@ -0,0 +1,1 @@
+module.exports = module.parent.children[0].exports
---
+++ 005_about_another_node_require/hello.js
@@ -0,0 +1,18 @@
+console.log(require('./x'))
+
+let x = require('./x')
+console.log(x.one)
+
+console.log('---------')
+
+console.log(require('./y'))
+
+console.log('---------')
+
+console.log(require('./z'))
+
+console.log('---------')
+console.log(require('./doesnot_work'))
+
+console.log('---------')
+console.log(require('./call_x'))
